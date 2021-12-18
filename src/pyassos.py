5d468cdea2c93425f5774df41c8190f3f4af8d94
Matthieu BOUCHET <matthieu.bouchet@outlook.com>
2021-12-18 23:31:55 +0100

    Documentation

--- src/pyassos.py
+++ src/pyassos.py
@@ -7,7 +7,7 @@ ADRESSE_API_INFO_RNA = ADRESSE_VERSION_RACINE + "id/"
 ADRESSE_API_INFO_SIRET = ADRESSE_VERSION_RACINE + "siret/"
 
 
-def recherche_par_nom(nom_cherche):
+def recherche_par_nom(nom_cherche: str) -> list or None:
     """Recherche via l'API des associations correspondantes au nom
     recherchées.
 
@@ -32,7 +32,7 @@ def recherche_par_nom(nom_cherche):
         listeAssociation.append(association)
 
     if json_data["total_pages"] > 1:
-        page=2
+        page = 2
         while page <= json_data["total_pages"] and page <= 60:
             requete_api = requests.get(ADRESSE_API_INFO_TEXTE +
                                        str(nom_cherche) +
@@ -68,15 +68,60 @@ class Association:
         # nomenclature nationale
         self.code_objet_social_2 = None  # 2ème code ( facultatif ) dans
         # la nomenclature nationale
-        self.adresse_voie = None  # Adresse de l'association
-        self.adresse_code_insee = None  # Code INSEE commune
-        self.adresse_code_postal = None  # Code postale de la commune
+        self.adresse = None  # Adresse de l'association
+        self.code_insee = None  # Code INSEE commune
+        self.code_postal = None  # Code postale de la commune
         self.commune = None  # Commune de l'association
         self.pays = None  # Pays de l'association
         self.telephone = None  # Téléphone de l'association
+        self.email = None  # Courriel de l'asssociation
         self.site_web = None  # Site web de l'association
         self.activite = None  # D: Dissoute;S:Supprimée;A:En activité
         self.derniere_maj = None  # Date de MAJ de article
+        self.observations = None  # Observations sur l'association
+
+        if params is not None:
+            if type(params) is not dict:
+                raise ValueError("Les paramètres de l'association n'est"
+                                 "pas un dictionnaire")
+
+    def set_attributs_api(self, donnees: dict):
+        self.id = donnees["id_association"]
+        self.id_ex = donnees["id_ex_association"]
+        self.siret = donnees["siret"]
+        self.num_utilite_publique = donnees["numero_reconnaissance"
+                                            "_utitlite_publique"]
+        self.code_gestion = donnees["code_gestion"]
+        self.date_creation = donnees["date_creation"]
+        self.date_derniere_declaration = donnees["date_derniere_"
+                                                 "declaration"]
+        self.date_publi_creation = donnees["date_publication_creation"]
+        self.date_publi_dissolution = donnees["date_declaration_"
+                                              "dissolution"]
+        self.nature = donnees["nature"]
+        self.groupement = donnees["groupement"]
+        self.titre = donnees["titre"]
+        self.titre_court = donnees["titre_court"]
+        self.objet_social = donnees["objet"]
+        self.code_objet_social_1 = donnees["objet_social1"]
+        self.code_objet_social_2 = donnees["objet_social2"]
+        self.code_postal = donnees["adresse_code_postal"]
+        self.code_insee = donnees["adresse_code_insee"]
+        self.commune = donnees["adresse_libelle_commune"]
+        self.pays = donnees["adresse_gestion_pays"]
+        self.adresse = "{}, {} {} {} {}".format(
+            donnees["adresse_gestion_geo"],
+            donnees["adresse_gestion_libelle_voie"],
+            self.code_postal,
+            self.commune,
+            self.pays
+        )
+        self.telephone = donnees["telephone"]
+        self.email = donnees["email"]
+        self.site_web = donnees["site_web"]
+        self.observations = donnees["observation"]
+        self.activite = donnees["position_acitivite"]
+        self.derniere_maj = donnees["derniere_maj"]
 
 
 
